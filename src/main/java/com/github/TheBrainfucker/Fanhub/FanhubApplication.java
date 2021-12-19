ea2cca1e774691ed02c9140166ab9e8b68fcb899
TheBrainfucker <42376412+TheBrainfucker@users.noreply.github.com>
2021-12-19 20:04:22 +0600

    Add spring initializr with dependencies

---
+++ src/main/java/com/github/TheBrainfucker/Fanhub/FanhubApplication.java
@@ -0,0 +1,13 @@
+package com.github.TheBrainfucker.Fanhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanhubApplication.class, args);
+	}
+
+}
